9b0bcb7a6e28f2ab03071270ba21b406742fcdf8
hikalium <hikalium@hikalium.com>
2019-06-27 16:28:47 +0900

    Add legacy example

---
+++ legacy/hello.cc
@@ -0,0 +1,13 @@
+// Based on https://dev.krzaq.cc/post/writing-cpp17-for-16bit-x86/
+void hello()
+{
+	// text mode text buffer begins at 0xB8000 
+  const char *s = "Hello, world!";
+	char* textBuffer = reinterpret_cast<char*>(0xB8000);
+  while(*s){
+	  *textBuffer = *s;
+    textBuffer += 2;
+    s++;
+  }
+  for(;;){}
+}
