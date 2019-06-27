bdb0cd94ea27f70c2db26be36f012916a4501dfc
hikalium <hikalium@hikalium.com>
2019-06-27 20:23:28 +0900

    Update

--- legacy_cpp/hello.cc
+++ legacy_cpp/hello.cc
@@ -1,8 +1,10 @@
 // Based on https://dev.krzaq.cc/post/writing-cpp17-for-16bit-x86/
 void hello() {
-  // text mode text buffer begins at 0xB8000
   const char* s = "Hello, world!";
   char* textBuffer = reinterpret_cast<char*>(0xB8000);
+  // This code will not run successfuly on the real machine
+  // because it is accessing addr 0xB8000 which is beyond the segment limit (64KiB)
+  // However, This code works on QEMU because QEMU does not check the segment limit every time...
   while (*s) {
     *textBuffer = *s;
     textBuffer += 2;
